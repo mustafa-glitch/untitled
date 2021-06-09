13f3ef27fa4daad5d8582f552915bbd412ed718e
Mustafa Ubeyde BUYUKAY <81827430+mustafa-glitch@users.noreply.github.com>
2021-06-10 00:32:53 +0300

    sec update

--- src/test/java/heytest.java
+++ src/test/java/heytest.java
@@ -1,6 +1,7 @@
 public class heytest {
     public static void main(String[] args) {
         System.out.println("hello test");
+        System.out.println("about to make more work on gut");
 
     }
 }
